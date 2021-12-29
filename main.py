47758d75708d6318174de9afa3bf14c34491ce2d
Aditya <92910224+Aditya-2407-G@users.noreply.github.com>
2021-12-29 06:32:53 +0000

    name

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ print("Welcome to the Brand Name Generator.")
 
 #2. Ask the user for the city that they grew up in.
 
-question1 = input("What's name of the city you grew up in?\n" )
+question1 = input("What's name of the city you grew up in?\n")
 
 #3. Ask the user for the name of a pet.
 
@@ -11,7 +11,7 @@ question2 = input("What's your pet's name?\n")
 
 #4. Combine the name of their city and pet and show them their band name.
 
-print ("Your band name could be " + question1 + " " + question2 )
+print("Your band name could be " + question1 + " " + question2)
 
 #5. Make sure the input cursor shows on a new line, see the example at:
 #   https://replit.com/@appbrewery/band-name-generator-end
